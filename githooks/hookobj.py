96f404549279f791db6c8ca36490ca8d592af6b5
Kevin Steffler <kevin5steffler@gmail.com>
2013-03-17 21:49:26 -0600

    Made logs default to w filemode

--- githooks/hookobj.py
+++ githooks/hookobj.py
@@ -8,7 +8,8 @@ from pykfs import gitlib
 
 _LOGGING_DEFAULT={"level": logging.INFO,
                   "format": '- %(asctime)s - %(levelname)s - %(message)s',
-                  "datefmt": '%Y-%m-%d %H:%M:%S' }
+                  "datefmt": '%Y-%m-%d %H:%M:%S',
+                  "filemode": 'w'}
 
 
 class GitHook(object):
