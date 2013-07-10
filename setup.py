7ad4610ccb5f1753a4a25db637513c0e89f4aad1
Kevin Steffler <kevin5steffler@gmail.com>
2013-07-10 12:35:04 -0600

    Added correct versioning

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='pykfs',
-    version='1.0',
+    version='0.1.0',
     description="Kevin Steffler's python library",
     author='Kevin Steffler',
     author_email='kevin5steffler@gmail.com',
